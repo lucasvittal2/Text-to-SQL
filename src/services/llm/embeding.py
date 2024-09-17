67d294dae532bec90f60331c618d75c7402b84c6
lucasvittal2 <lucasvittal@gmail.com>
2024-09-17 13:39:24 -0300

    adjustment inserting get_params to get parameter from database dynamically

--- src/services/llm/embeding.py
+++ src/services/llm/embeding.py
@@ -10,27 +10,34 @@ class EmbeddingGenerator:
     def __init__(self, embedding_model:str, api_key: str) -> None:
         self.embedding_model = embedding_model
         self.api_key = api_key
-        self.parms = Config().getConfig()
+        
         
     def getEmbedding(text:str) -> List[float]:
         raise NotImplemented("Method 'getEmbedding' were not implemented by 'EmbeddingGenerator' child")
+    
+    def __getParams(self) -> None:
+        raise NotImplemented("Method '__getParams' were not implemented by 'EmbeddingGenerator' child")
 
 class OpenAIEmbeddingGenerator(EmbeddingGenerator):
     
     def __init__(self) -> None:
 
-        embedding_model = self.parms["OPENAI_EMBEDDING_MODEL"]
-        api_key=self.parms["OPENAI_KEY"]
-        super().__init__(  embedding_model, api_key )
+        self.__getParams()
         self.client = OpenAI(api_key= self.api_key)
         
     def getEmbedding(self, text:str) -> List[float]:
         
         try:
-            
+            self.__getParams()
             embedding = self.client.embeddings.create(input = [text], model=self.embedding_model).data[0].embedding
             logging.info(f"The text {text}' were transformed to OpenAI embedding.")
             return embedding
         except Exception as err:
             logging.error(f"OpenAI embedding generation has failed due to the following error: \n\n{err}\n\n")
             raise err
+
+    def __getParams(self) -> None:
+        parms = Config().getConfig()
+        embedding_model = parms["OPENAI_EMBEDDING_MODEL"]
+        api_key= parms["OPENAI_KEY"]
+        super().__init__(embedding_model, api_key )
